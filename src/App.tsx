6bbaa7d76ddca5b36c1f124053fc5359093e67e4
Ramona <rmn.neagu@yahoo.ro>
2026-08-27 19:47:17 +0300

    feat: build the header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Header, Title, PropertyList, Footer } from './components'
+import styles from './App.module.css'
 
 export default function App() {
   return (
-    <>
+    <div className={styles.app}>
       <Header />
       <main>
         <Title>Cambridge Rentals</Title>
         <PropertyList />
       </main>
       <Footer />
-    </>
+    </div>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,21 @@
 import styles from './Header.module.css'
+import { House, Phone, Mail } from 'lucide-react';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-    
+      <div className={`${styles.item} ${styles.brand}`}>
+        <House className={styles.icon}/>
+        <span className={styles.span}>Cambridge Rentals</span>
+      </div>
+      <div className={`${styles.item} ${styles.contact}`}>
+        <Phone className={styles.icon} />
+        <span className={styles.span}>(555) 123-4567 </span>
+      </div>
+       <div className={`${styles.item} ${styles.contact}`}>
+        <Mail className={styles.icon}/>
+        <span className={styles.span}>support@cambridgerentals.com </span>
+      </div>
     </header>
   )
 }
